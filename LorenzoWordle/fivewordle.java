7c6c0c4333662d8c93136f3910e91526d188ae89
lorenzoRomero <132471059+lorenzoRomero@users.noreply.github.com>
2024-10-07 16:06:37 -0500

    Create fivewordle.java

---
+++ LorenzoWordle/fivewordle.java
@@ -0,0 +1,24 @@
+public class fiveWordle
+{
+  private String wordInserted;
+  private String answer = bread;
+  private int amountOfTries;
+  private int amountOfLetters;
+   int getTriesLeft(){
+       return 6 - amountOfTries;
+   }
+   void insertWord(String word)
+       {
+           wordInserted = word;
+       }
+   String getWord(){
+       return answer;
+   }
+   
+   Boolean compareWords(){
+       return wordInserted = answer;
+   }
+   int getAmountLetters(){
+       return 5;
+   }
+}
