c6162c0653bcea7ee96796e68a15ef65f76480b1
lorenzoRomero <132471059+lorenzoRomero@users.noreply.github.com>
2024-10-07 16:07:10 -0500

    Create sixwordle.java

---
+++ LorenzoWordle/sixwordle.java
@@ -0,0 +1,24 @@
+public class sixWordle
+{
+     private String wordInserted;
+  private String answer = tomato;
+  private int amountOfTries;
+  private int amountOfLetters;
+   int getTriesLeft(){
+       
+   }
+   void insertWord(String word)
+       {
+           wordInserted = word;
+       }
+   String getWord(){
+         return answer;
+   }
+   
+   Boolean compareWords(){
+       return wordInserted = answer;
+   }
+   int getAmountLetters(){
+       return 5;
+   }
+}
