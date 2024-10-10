2ae34262c71a7f7e77970a2e81aead0239141fbc
jackpriestley <119704841+jackpriestley@users.noreply.github.com>
2024-10-09 19:58:14 -0500

    Add files via upload

---
+++ Services.java
@@ -0,0 +1,8 @@
+interface Services
+{
+    void getLastTireServices();
+    void getLastEngineServices();
+    int nextTireServices(int serviceMiles);
+    int nextEnginerServces(int serviceMiles);
+    String nextServices();
+}
